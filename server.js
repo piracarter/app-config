5083548e505c626da14c82c70cc5aa0c8d3241cc
Nagios <iaasautomationamerica.group@bbva.com>
2018-05-17 17:56:06 -0500

    first commit 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.listen(8080,function(){
+
+console.log("server is up and running at port 8080");
+})
+
+app.get('/', function(request,response){
+response.send("Hello World");
+responde.send(process.env.APP_MSG);
+})
